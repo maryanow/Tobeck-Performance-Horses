0f345b826cfcaa4b44979c2f7581897fd2f8d450
Iain Maryanow <imaryanow@yahoo.com>
2016-03-31 19:36:13 -0700

    Add new controller

---
+++ public/modules/core/controllers/sales.client.controller.js
@@ -0,0 +1,58 @@
+'use strict';
+
+angular.module('core').controller('SalesController', ['$scope', '$location', '$http', 'Authentication', '$sce',
+    function($scope, $location, $http, Authentication, $sce) {
+        $scope.authentication = Authentication;
+
+        $scope.editing = false;
+
+        $http.get('/posts').success(function(data) {
+            delete $scope.error;
+            $scope.posts = data;
+        }).error(function(err) {
+            $scope.posts = null;
+            $scope.error = err.message;
+        });
+
+        $http.get('/pages/sales').success(function(data) {
+            delete $scope.error;
+            $scope.page = data;
+        }).error(function(err) {
+            $scope.error = err.message;
+            $scope.page = null;
+        });
+
+        /*
+        *   Transform URL into trusted URL for iframe/img use.
+        */
+        $scope.getResource = function(src) {
+            return $sce.trustAsResourceUrl(src);
+        }
+
+        $scope.editPage = function() {
+            $scope.editing = true;
+        }
+
+        $scope.cancelEdit = function() {
+            $scope.editing = false;
+        }
+
+        $scope.savePage = function() {
+            $http.post('/pages/save', $scope.page).success(function() {
+                delete $scope.error;
+            }).error(function(response) {
+                $scope.error = response.message;
+            });
+
+            $scope.editing = false;
+        }
+
+        $scope.addItem = function(index, _desc) {
+            $scope.page.data.splice(index + 1, 0, {desc: _desc, value: ''});
+        }
+
+        $scope.removeItem = function(item) {
+            $scope.page.data.splice($scope.page.data.indexOf(item), 1);
+        }
+    }
+]);
